e8b9948b787750a7bad35a8a60ce285a9b00da9d
jwells52 <136543691+jwells52@users.noreply.github.com>
2023-06-23 19:18:28 +0000

    Fixing pylint errors

--- Module 4/jwells52_assignment3.py
+++ Module 4/jwells52_assignment3.py
@@ -17,30 +17,35 @@ RESULT_INDICES = [
     9351
 ] # Indices where there is a big change in tokens when lemmatization or stemming is applied
 
-def write_results(sentences, tokens, stemmed_tokens, lemma_tokens):
-    results_file = 'output/jwells52_assignment3/results.txt'
+def write_results(
+          _sentences, 
+          _tokens, 
+          _stemmed_tokens, 
+          _lemma_tokens):
+    '''Function for writing results of tokenization, stemming, and lemmatization to .txt file'''
+    results_file = '/output/results.txt'
 
     print(f'Writing results to {results_file}...')
-    with open(results_file, 'w') as f:
+    with open(results_file, "w", encoding="utf8") as f:
         for i in RESULT_INDICES:
-                print(f'Index: {i}', file=f)
-                print('UNPROCESSED STRING', file=f)
-                print(sentences[i], file=f)
-                print(file=f)
+            print(f'Index: {i}', file=f)
+            print('UNPROCESSED STRING', file=f)
+            print(_sentences[i], file=f)
+            print(file=f)
 
 
-                print('TOKENIZATION RESULTS', file=f)
-                print(tokens[i], file=f)
-                print(file=f)
+            print('TOKENIZATION RESULTS', file=f)
+            print(_tokens[i], file=f)
+            print(file=f)
 
-                print('STEMMING RESULTS', file=f)
-                print(stemmed_tokens[i], file=f)
-                print(file=f)
+            print('STEMMING RESULTS', file=f)
+            print(_stemmed_tokens[i], file=f)
+            print(file=f)
 
-                print('LEMMATIZATION RESULTS', file=f)
-                print(lemma_tokens[i], file=f)
-                print('='*100, file=f)
-                print(file=f)
+            print('LEMMATIZATION RESULTS', file=f)
+            print(_lemma_tokens[i], file=f)
+            print('='*100, file=f)
+            print(file=f)
 
 if __name__ == "__main__":
     print('Loading data...')
