8e7c7aa0fb43c08e9da6912393019d16ff708dbb
jwells52 <136543691+jwells52@users.noreply.github.com>
2023-07-26 15:50:41 -0600

    Update plotting.py

--- Research Project/modules/plotting.py
+++ Research Project/modules/plotting.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-def fsl_plots(dict, title, xlabel='Epochs', ylabel='Loss', save_plot=False, save_path=None):
+def fsl_plots(dict, title, parameter='way', xlabel='Epochs', ylabel='Loss', save_plot=False, save_path=None):
   fig, ax = plt.subplots()
 
   plt.title(title)
 
   for key, values in dict.items():
-    ax.plot(values, label=f'{key}-shot')
+    ax.plot(values, label=f'{key}-{parameter}')
 
   ax.set_xlabel(xlabel)
   ax.set_ylabel(ylabel)
