c53c363d1fe38b5899c579dc14cf24647b8b32d0
jwells52 <136543691+jwells52@users.noreply.github.com>
2023-07-31 20:17:57 -0600

    Update data_utils.py

--- Research Project/modules/data_utils.py
+++ Research Project/modules/data_utils.py
@@ -46,30 +46,18 @@ def filter_low_occuring_classes(df:pd.DataFrame, threshold:int=10):
   return _df
 
 
-def create_loaders(train_set, valid_set, n_way, n_shot, n_query, n_tasks_training, n_tasks_validation, num_workers=2):
+def create_loader(dataset, n_way, n_shot, n_query, n_tasks, num_workers=2):
     
-  train_sampler = TaskSampler(
-      train_set, n_way=n_way, n_shot=n_shot, n_query=n_query, n_tasks=n_tasks_training
+  sampler = TaskSampler(
+      dataset, n_way=n_way, n_shot=n_shot, n_query=n_query, n_tasks=n_tasks
   )
 
-  valid_sampler = TaskSampler(
-      valid_set, n_way=n_way, n_shot=n_shot, n_query=n_query, n_tasks=n_tasks_validation
-  )
-
-  train_loader = DataLoader(
+  loader = DataLoader(
       train_set,
-      batch_sampler=train_sampler,
+      batch_sampler=sampler,
       num_workers=num_workers,
       pin_memory=True,
       collate_fn=train_sampler.episodic_collate_fn
   )
 
-  valid_loader = DataLoader(
-      valid_set,
-      batch_sampler=valid_sampler,
-      num_workers=num_workers,
-      pin_memory=True,
-      collate_fn=valid_sampler.episodic_collate_fn
-  )
-
-  return train_loader, valid_loader
+  return loader
